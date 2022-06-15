e5e5c3e839363adc18201578aa19eac55314c537
Alec Price <52548136+Alecprice@users.noreply.github.com>
2022-06-15 16:10:41 -0500

    Add files via upload

---
+++ CaesarsCipher.js
@@ -0,0 +1,44 @@
+function rot13(str) {
+  var cipherCode = {
+    A: "N",
+    B: "O",
+    C: "P",
+    D: "Q",
+    E: "R",
+    F: "S",
+    G: "T",
+    H: "U",
+    I: "V",
+    J: "W",
+    K: "X",
+    L: "Y",
+    M: "Z",
+    N: "A",
+    O: "B",
+    P: "C",
+    Q: "D",
+    R: "E",
+    S: "F",
+    T: "G",
+    U: "H",
+    V: "I",
+    W: "J",
+    X: "K",
+    Y: "L",
+    Z: "M",
+    " ": " ",
+    ".": ".",
+    "!": "!",
+    "?": "?",
+  };
+
+  return str
+    .split("")
+    .map((char) => cipherCode[char])
+    .join("");
+}
+
+rot13("SERR PBQR PNZC!");
+rot13("SERR CVMMN!");
+rot13("SERR YBIR?");
+rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.");
